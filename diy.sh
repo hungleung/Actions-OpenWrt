05bfcead8cfc7873717872da6f86844522115556
hungleung <hungleung.lee@me.com>
2020-04-01 16:00:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,6 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 
-
 git clone https://github.com/mchome/openwrt-vlmcsd.git ./package/openwrt-vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git ./package/luci-app-vlmcsd
 git clone https://github.com/kuoruan/openwrt-v2ray.git ./package/openwrt-v2ray
