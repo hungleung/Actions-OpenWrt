6e2e82edeb33d957e80eaae279ce74efdc7b42f0
hungleung <hungleung.lee@me.com>
2020-04-29 14:15:36 +0800

    change default package

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,22 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.252/g' package/base-files/files/bin/config_generate
+
+# Default packages - the really basic set
+sed -i 's/block-mount/curl bind-dig htop/g' include/target.mk
+sed -i 's/luci-app-adbyby-plus luci-app-autoreboot/luci-ssl-openssl/g' include/target.mk
+sed -i 's/luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic/luci-app-ssr-plus/g' include/target.mk
+sed -i 's/luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree/luci-app-vlmcsd/g' include/target.mk
+sed -i 's/luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq/luci-app-sfe luci-app-flowoffload luci-app-cpufreq/g' include/target.mk
+sed -i 's/ddns-scripts_aliyun ddns-scripts_dnspod/luci-app-udpxy luci-app-acme acme-dnsapi/g' include/target.mk
+
+sed -i 's/kmod-ath5k kmod-ath9k kmod-ath9k-htc kmod-ath10k kmod-rt2800-usb kmod-e1000e kmod-igb kmod-igbvf kmod-ixgbe kmod-pcnet32 kmod-tulip kmod-vmxnet3 kmod-i40e kmod-i40evf kmod-r8125 kmod-8139cp kmod-8139too kmod-fs-f2fs \\//g' target/linux/x86/Makefile
+sed -i 's/htop lm-sensors autocore automount autosamba luci-app-ipsec-vpnd luci-proto-bonding luci-app-unblockmusic luci-app-zerotier luci-app-xlnetacc ddns-scripts_aliyun ddns-scripts_dnspod ca-certificates \\//g' target/linux/x86/Makefile
+sed -i 's/luci-app-airplay2 luci-app-music-remote-center luci-app-qbittorrent luci-app-amule luci-app-openvpn-server \\//g' target/linux/x86/Makefile
+sed -i 's/ath10k-firmware-qca988x ath10k-firmware-qca9888 ath10k-firmware-qca9984 brcmfmac-firmware-43602a1-pcie \\//g' target/linux/x86/Makefile
+sed -i 's/kmod-sound-hda-core kmod-sound-hda-codec-realtek kmod-sound-hda-codec-via kmod-sound-via82xx kmod-sound-hda-intel kmod-sound-hda-codec-hdmi kmod-sound-i8x0 kmod-usb-audio \\//g' target/linux/x86/Makefile
+sed -i 's/kmod-usb-net kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152\\//g' target/linux/x86/Makefile
+
+sed -i 's/kmod-e1000e kmod-e1000 kmod-r8169 kmod-igb kmod-bnx2/kmod-e1000e/g' target/linux/x86/64/target.mk
+sed -i 's/squashfs vdi vmdk pcmcia fpu/squashfs vdi vmdk pcmcia fpu vhd/g' target/linux/x86/Makefile
