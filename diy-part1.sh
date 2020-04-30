996553049ad0a76d11027c5e65d75446271e1ace
hungleung <hungleung.lee@me.com>
2020-04-30 16:00:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/hungleung/lede
 pwd
+ls -la
 cp -R ./lede/package/lean/luci-app-ssr-plus ../openwrt/package/lean/
