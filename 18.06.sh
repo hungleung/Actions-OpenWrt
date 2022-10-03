5182af66425f42baa87a4985dea51b66dc5d9ee6
hungleung <hungleung.lee@me.com>
2022-10-03 23:19:13 +0800

    Update 18.06.sh

--- 18.06.sh
+++ 18.06.sh
@@ -12,14 +12,6 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i 's/ImmortalWrt/Home/g' package/base-files/files/bin/config_generate
 
 # disable wireless components
-sed -i 's/\+libiwinfo-lua//' feeds/luci/collections/luci/Makefile
-sed -i 's/\+libiwinfo//' feeds/luci/modules/luci-mod-dashboard/Makefile
-sed -i 's/\+libiwinfo-lua//' feeds/luci/modules/luci-mod-battstatus/Makefile
-sed -i 's/\+rpcd-mod-iwinfo//' feeds/luci/modules/luci-mod-battstatus/Makefile
-sed -i 's/\+libiwinfo-lua//' feeds/luci/modules/luci-mod-network/Makefile
-sed -i 's/\+rpcd-mod-iwinfo//' feeds/luci/modules/luci-mod-network/Makefile
-sed -i 's/\+libiwinfo-lua//' feeds/luci/modules/luci-mod-status/Makefile
-sed -i 's/\+libiwinfo//' feeds/luci/modules/luci-mod-status/Makefile
 sed -i 's/wpad-basic-openssl//' target/linux/ramips/mt7621/target.mk
 
 # disable multi pppoe
@@ -28,12 +20,10 @@ sed -i 's/\+shellsync//' package/network/services/ppp/Makefile
 sed -i 's/\+kmod-mppe//' package/network/services/ppp/Makefile
 
 # change menu title
-sed -i 's/"title": "udpxy",/"title": "IPTV",/' feeds/luci/applications/luci-app-udpxy/root/usr/share/luci/menu.d/luci-app-udpxy.json
 sed -i 's/"ShadowSocksR Plus+"/"SSRP+"/'  feeds/luci/applications/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 
 # change default firmware size and package
-sed -i '95,105d' target/linux/ramips/dts/mt7621_jdcloud_re-sp-01b.dts
-sed -i -e 's/27328k/32448k/' -e 's/kmod-fs-ext4 kmod-mt7603 kmod-mt7615e/lsblk kmod-fs-ext4 e2fsprogs fdisk/' -e 's/kmod-mt7615-firmware kmod-sdhci-mt7620 kmod-usb3/kmod-sdhci-mt7620/' target/linux/ramips/image/mt7621.mk
+sed -i -e 's/kmod-mt7603 kmod-mt7615e/lsblk e2fsprogs fdisk/' -e 's/wpad-openssl//g' target/linux/ramips/image/mt7621.mk
 
 # change default package
 sed -i 's/luci-app-filetransfer/luci-app-udpxy luci-app-ssr-plus luci-app-vlmcsd luci-app-upnp luci-app-wireguard/' include/target.mk
